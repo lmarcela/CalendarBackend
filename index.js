97d6be343920920d52caba8235b6a3188a696754
Marcela Malaver <marcela9409@gmail.com>
2020-08-24 18:48:13 -0500

    Rutas de usuarios o auth

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 
 //Crear el servidor de express
 const app = express();
 
-//Rutas
-/* app.get('/', (req, res)=>{
-    res.json({
-        ok: true
-    })
-}) */
-
 //Directorio publico
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+//Rutas
+app.use("/api/auth", require("./routes/auth"));
 
 //Escuchar peticiones
-app.listen(process.env.PORT,()=>{
-    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
