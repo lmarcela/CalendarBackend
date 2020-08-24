355da20d80ab1de0a6007b53272f3e7254ec6d72
Marcela Malaver <marcela9409@gmail.com>
2020-08-24 18:25:23 -0500

    Configurando express

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hola mundo!!!");
+const express = require('express');
+//Crear el servidor de express
+const app = express();
+
+//Rutas
+app.get('/', (req, res)=>{
+    console.log('Se requiere /');
+    res.json({
+        ok: true
+    })
+})
+
+//Escuchar peticiones
+app.listen(3001,()=>{
+    console.log(`Servidor corriendo en puerto ${3001}`);
+})
