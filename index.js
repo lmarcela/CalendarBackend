7844fcb75a0c1aedd034a483e0533e11d7632f98
Marcela Malaver <marcela9409@gmail.com>
2020-08-24 18:37:40 -0500

    Cariable de entorno y carpeta publica

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require('express');
+require('dotenv').config();
+
 //Crear el servidor de express
 const app = express();
 
 //Rutas
-app.get('/', (req, res)=>{
-    console.log('Se requiere /');
+/* app.get('/', (req, res)=>{
     res.json({
         ok: true
     })
-})
+}) */
+
+//Directorio publico
+app.use(express.static('public'));
 
 //Escuchar peticiones
-app.listen(3001,()=>{
-    console.log(`Servidor corriendo en puerto ${3001}`);
+app.listen(process.env.PORT,()=>{
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 })
