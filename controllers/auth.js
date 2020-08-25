62c49526742b07a6776a520b3c6772ea7bf12c6d
Marcela Malaver <marcela9409@gmail.com>
2020-08-24 19:47:45 -0500

    Express Validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,18 @@
 const { response } = require("express");
+const { validationResult } = require("express-validator");
 
 const crearUsuario = (req, res = response) => {
   const { name, email, password } = req.body;
-  if (name.length < 5) {
+
+  //manejo de errores
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
     return res.status(400).json({
       ok: false,
-      msg: "El nombre debe ser de 5 letras",
+      errors: errors.mapped(),
     });
   }
-  res.json({
+  res.status(201).json({
     ok: true,
     name,
     email,
@@ -18,7 +22,16 @@ const crearUsuario = (req, res = response) => {
 
 const loginUsuario = (req, res) => {
   const { email, password } = req.body;
-  res.json({
+
+  //manejo de errores
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped(),
+    });
+  }
+  res.status(201).json({
     ok: true,
     email,
     password,
--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ Rutas de usuarios / Auth
 host + /api/auth 
 */
 const { Router } = require("express");
+const { check } = require("express-validator");
 const router = Router();
 const {
   crearUsuario,
@@ -10,9 +11,24 @@ const {
   revalidarToken,
 } = require("../controllers/auth");
 
-router.post("/new", crearUsuario);
+router.post(
+  "/new",
+  [
+    //Middlewares
+    check("name", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe de ser de 6 caracteres").isLength({min:6})
+  ],
+  crearUsuario
+);
 
-router.post("/", loginUsuario);
+router.post("/",
+[
+  //Middlewares
+  check("email", "El email es obligatorio").isEmail(),
+  check("password", "El password debe de ser de 6 caracteres").isLength({min:6})
+],
+loginUsuario);
 
 router.get("/renew", revalidarToken);
 
