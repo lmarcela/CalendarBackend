f9256874f187fa12fe56ddc7186b5ca1dc6d98ff
Marcela Malaver <marcela9409@gmail.com>
2020-08-24 20:01:14 -0500

    Custom Middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,8 @@
 const { response } = require("express");
-const { validationResult } = require("express-validator");
 
 const crearUsuario = (req, res = response) => {
   const { name, email, password } = req.body;
 
-  //manejo de errores
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
   res.status(201).json({
     ok: true,
     name,
@@ -22,15 +13,7 @@ const crearUsuario = (req, res = response) => {
 
 const loginUsuario = (req, res) => {
   const { email, password } = req.body;
-
-  //manejo de errores
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped(),
-    });
-  }
+  
   res.status(201).json({
     ok: true,
     email,
@@ -46,5 +29,5 @@ const revalidarToken = (req, res) => {
 module.exports = {
   crearUsuario,
   loginUsuario,
-  revalidarToken,
+  revalidarToken
 };
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ host + /api/auth
 */
 const { Router } = require("express");
 const { check } = require("express-validator");
+const {validarCampos} = require("../middlewares/validar-campos");
+
 const router = Router();
 const {
   crearUsuario,
@@ -17,18 +19,26 @@ router.post(
     //Middlewares
     check("name", "El nombre es obligatorio").not().isEmpty(),
     check("email", "El email es obligatorio").isEmail(),
-    check("password", "El password debe de ser de 6 caracteres").isLength({min:6})
+    check("password", "El password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+    validarCampos
   ],
   crearUsuario
 );
 
-router.post("/",
-[
-  //Middlewares
-  check("email", "El email es obligatorio").isEmail(),
-  check("password", "El password debe de ser de 6 caracteres").isLength({min:6})
-],
-loginUsuario);
+router.post(
+  "/",
+  [
+    //Middlewares
+    check("email", "El email es obligatorio").isEmail(),
+    check("password", "El password debe de ser de 6 caracteres").isLength({
+      min: 6,
+    }),
+    validarCampos
+  ],
+  loginUsuario
+);
 
 router.get("/renew", revalidarToken);
 
