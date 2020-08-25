2d1c9ae21296a8b181ec111a9fa07e147ac483ba
Marcela Malaver <marcela9409@gmail.com>
2020-08-25 18:04:33 -0500

    Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,15 +30,33 @@ const crearUsuario = async (req, res = response) => {
   }
 };
 
-const loginUsuario = (req, res) => {
+const loginUsuario = async (req, res) => {
   const { email, password } = req.body;
+  try {
+    const usuario = await Usuario.findOne({ email });
+    if (!usuario) {
+      return res
+        .status(400)
+        .json({ ok: false, msg: "No existe un usuario con ese correo" });
+    }
+    //Confirmar los passwords
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(400).json({ ok: false, msg: "Password incorrecto" });
+    }
 
-  res.status(201).json({
-    ok: true,
-    email,
-    password,
-  });
+    //Generar JWT
+    res.status(201).json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ ok: false, msg: "Valide con el administrador" });
+  }
 };
+
 const revalidarToken = (req, res) => {
   res.json({
     ok: true,
