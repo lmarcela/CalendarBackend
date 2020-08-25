bbc119621d6adddaa4e936c8ff2856c654ea155d
Marcela Malaver <marcela9409@gmail.com>
2020-08-24 19:24:55 -0500

    Recuperar información de un posteo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,27 @@
 const { response } = require("express");
 
 const crearUsuario = (req, res = response) => {
+  const { name, email, password } = req.body;
+  if (name.length < 5) {
+    return res.status(400).json({
+      ok: false,
+      msg: "El nombre debe ser de 5 letras",
+    });
+  }
   res.json({
     ok: true,
+    name,
+    email,
+    password,
   });
 };
 
 const loginUsuario = (req, res) => {
+  const { email, password } = req.body;
   res.json({
     ok: true,
+    email,
+    password,
   });
 };
 const revalidarToken = (req, res) => {
