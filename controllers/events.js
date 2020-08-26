aacfb4779d01e95d421a75119eec6778bf8ec3b3
Marcela Malaver <marcela9409@gmail.com>
2020-08-26 12:30:40 -0500

    CRUD de events

---
+++ controllers/events.js
@@ -0,0 +1,39 @@
+/* {
+    ok: true,
+    msg: 'Obtener eventos'
+} */
+
+const { response } = require("express");
+
+const getEventos = async (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: "getEventos",
+  });
+};
+
+const crearEvento = async (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: "crearEvento",
+  });
+};
+const actualizarEvento = async (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: "actualizarEvento",
+  });
+};
+const eliminarEvento = async (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: "eliminarEvento",
+  });
+};
+
+module.exports = {
+  getEventos,
+  crearEvento,
+  actualizarEvento,
+  eliminarEvento,
+};
---
+++ routes/events.js
@@ -0,0 +1,25 @@
+/* 
+Rutas de usuarios / Events 
+host + /api/events 
+*/
+const { Router } = require("express");
+
+const {
+  getEventos,
+  crearEvento,
+  actualizarEvento,
+  eliminarEvento,
+} = require("../controllers/events");
+
+const { validarJWT } = require("../middlewares/validar-jwt");
+
+const router = Router();
+router.use(validarJWT);
+
+router.get("/", getEventos);
+router.post("/", crearEvento);
+router.put("/:id", actualizarEvento);
+router.delete("/:id", eliminarEvento);
+
+module.exports = router;
+//VIDEO 339 MINUTO 4:38
