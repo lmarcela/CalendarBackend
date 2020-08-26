eabb18f77cac2352df251a1d8ffe7aeb1424e839
Marcela Malaver <marcela9409@gmail.com>
2020-08-25 19:58:49 -0500

    Configurar CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { dbConnection } = require("./database/config");
 require("dotenv").config();
 
@@ -7,6 +8,9 @@ const app = express();
 
 //BD
 dbConnection();
+
+//Cors
+app.use(cors());
 
 //Directorio publico
 app.use(express.static("public"));
