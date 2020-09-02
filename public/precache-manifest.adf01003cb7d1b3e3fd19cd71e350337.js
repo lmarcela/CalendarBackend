3cf0b9517088e7eaf08962b83a6b81cda307ed14
Marcela Malaver <marcela9409@gmail.com>
2020-09-01 19:18:01 -0500

    v1 del calendar-app

---
+++ public/precache-manifest.adf01003cb7d1b3e3fd19cd71e350337.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0e02d532ded15d11c4b189ef65b6ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b54cec6636242a6ac82",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "8b05402a540c48703de4",
+    "url": "/static/css/main.d2e2edec.chunk.css"
+  },
+  {
+    "revision": "4b54cec6636242a6ac82",
+    "url": "/static/js/2.0adde5c8.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.0adde5c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b05402a540c48703de4",
+    "url": "/static/js/main.0c166da3.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
